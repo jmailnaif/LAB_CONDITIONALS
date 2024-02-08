e6cad2394ab80469d07530e332439edddfc51180
jmail <jamailotb15@gmail.com>
2024-02-08 13:06:51 +0300

    solved

---
+++ lab1.py
@@ -0,0 +1,9 @@
+wieght = input("your wieght: ")
+height =input ("your height:")
+BML = wieght/height ** 2
+if BML>=50 :
+    print ("you are overwieght you need to work")
+elif BML <=45 :
+    print ("you are fit &healthy")
+elif BML <40 :
+    print("you are underweght. watch your health")
